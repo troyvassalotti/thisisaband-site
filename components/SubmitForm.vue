7c5d767a196db2fad4e674585e668abc8ab416fe
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-06-02 18:33:35 -0400

    fix to form 2

--- components/SubmitForm.vue
+++ components/SubmitForm.vue
@@ -3,7 +3,8 @@
     <h3>Submit a band</h3>
     <p>Use this form to submit a band to the site. You don't have to give me a name or email, but then I won't be able to attribute the post to you nor would I be able to contact you if there's something weird about your submission.</p>
     <p>Include as much information as you'd like in the submission box. Write your story, include a link to a video or song, etc. Please don't write a novel.</p>
-    <form class="submission" method="POST" name="Band Submission" netlify netlify-honeypot="bot-field">
+    <form class="submission" method="POST" name="Band Submission" data-netlify='true' data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="Band Submission" />
       <label style="display: none;">Don’t fill this out: <input name="bot-field"></label>
       <label for="name">Name (optional)<input id='name' name='name' placeholder='Cool Person' type='text'></label>
       <label for="email">Email (optional)<input id="email" name="email" type="email" placeholder='you@are.cool'></label>
