1b83e987d13a498b67f0468f4a6785637bc1e786
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-06-05 07:38:05 -0400

    moved to a system font stack

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,12 +42,7 @@ export default {
         content: 'summary_large_image'
       }
     ],
-    link: [{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap'
-      },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    link: [{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/site.webmanifest' }]
