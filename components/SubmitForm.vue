9456fbbc62b96874a20f05502d9284fcdbaf9017
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-06-05 07:25:15 -0400

    add github issue for submissions

--- components/SubmitForm.vue
+++ components/SubmitForm.vue
@@ -3,6 +3,8 @@
     <h3>Want to submit a band?</h3>
     <p>Use this form to submit a band to the site. You don't have to give me a name or email, but by not doing so I won't be able to attribute the post to you nor would I be able to contact you if there's something weird about your submission.</p>
     <p>Include as much information as you'd like in the submission box. Write your story, include a link to a video or song, etc. Please don't write a novel.</p>
+    <p>If you like working in GitHub and hate this form, you can also request or submit a band by <a
+      href='https://github.com/troyvassalotti/thisisaband-site/issues'>filing an issue</a>.</p>
     <form class="submission" method="POST" name="Band Submission" data-netlify='true' data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="Band Submission" />
       <label style="display: none;">Don’t fill this out: <input name="bot-field"></label>
@@ -69,5 +71,9 @@ form {
     background: var(--black-2);
     color: #fff;
   }
+}
+
+a {
+  text-decoration: underline;
 }
 </style>
